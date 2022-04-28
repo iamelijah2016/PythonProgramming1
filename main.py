6f98b97fd5aefd5c7fcd100996a48cc65897a26d
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 00:54:12 +0800

    4.8 Coding Exercise

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-x = 332
-y = 2031
+# for loop coding exercise
+furniture = ["table", "chair", "cabinet", "desk", "couch"]
 
-if x >= y:
-    print("x is greater than or equal to y.")
-elif x == y:
-    print("x is equal to y.")
+for i in furniture:
+    if i == "cabinet":
+        continue
+    print(i)
+
+
+# while loop coding exercise
+i = 1
+while i < 15:
+    print(i)
+    i += 1
 else:
-    print("x is less than y.")
+    print("i is no longer less than 15.")
