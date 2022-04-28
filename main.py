1c2b2bfe613ba16baa5a649872b828bc03ebcf12
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-28 22:58:55 +0800

    2.7 Coding Exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet_function(name):
-    print("Have a nice day, " + name + "!")
-
-
-greet_function("Junrey")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+favorite_color = input("Enter your favorite color: ")
+favorite_movie = input("Enter your favorite movie: ")
+mobile_number = input("Enter your 11-digit mobile number: ")
+motto_in_life = input("Enter your motto in life: ")
