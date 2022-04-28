2b87278a10fdb7b9c641744e8af5d11627776931
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-28 22:30:37 +0800

    1.14 Coding Exercise

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# character_job = "pilot"
-# character_ride = "plane"
-# character_souvenir = "fruits"
-# character_pet = "lion"
+print("Roses are red, violets are blue.")
 
-character_job = "pirate"
-character_ride = "ship"
-character_souvenir = "gold"
-character_pet = "parrot"
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less than forty-two!")
 
-print("There was once a " + character_job + " who loved adventure.")
-print("The " + character_job + " would take her " + character_ride + " to unknown places.")
-print("She brings home a lot of " + character_souvenir + ".")
-print("Then she goes home to her pet " + character_pet + ", Chuckles.")
+print("A long time ago in a galaxy far, far away...")
