a9d2cee5a3b41d2c8b467569c19d39b7eaf7eee9
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 00:03:06 +0800

    3.8 Coding Exercise

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-class Customers:
-    greeting = "Welcome to the Coffee Palace!"
-
-c_1 = Customers()
-c_1.name = "Samirah"
-c_1.beverage = "Iced caffe latte"
-c_1.food = "Cinnamon roll"
-c_1.total = 225
-
-c_2 = Customers()
-c_2.name = "Jerry"
-c_2.beverage = "Caramel macchiato"
-c_2.food = "Glazed doughnut"
-c_2.total = 230
-
-print(Customers.greeting)
-print(c_1.beverage)
-print(c_2.food)
+print(217 * 6)
+print(600 + 35234)
+print(67 // 12)
+print(56329 % 982)
+print(34 ** 5)
